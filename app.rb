de4a2ec02cd6f9bc0a06bce1a0e61558c96d9c02
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-25 23:54:20 +0300

    updateLogger

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
 class Logger
+
+    def initialize
+        @f = File.open 'log.txt', 'a'
+    end
+
+    # class method
     def self.say_something
         puts 'haha'
+    end
+
+    #instance method
+    def log_something wat
+        @f.puts wat
+        @f.close
     end
 end      
 
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something 'wazzuuppp'
