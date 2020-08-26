cba46dcb1946e7fefc677a9e50332ce17a2d2600
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-26 14:04:40 +0300

    updateLogger

--- app.rb
+++ app.rb
@@ -4,6 +4,16 @@ class Logger
         @f = File.open 'log.txt', 'a'
     end
 
+    @@x = nil
+
+    def self.instance
+        if @@x == nil
+            @xx = Logger.new
+        end
+
+        return @xx
+    end
+
     # class method
     def self.say_something
         puts 'haha'
@@ -18,6 +28,19 @@ end
 
 
 Logger.say_something
+Logger.instance.log_something 'dwhakdawdhuia'
+
+#Logger.instance.log_something = @@x.log_something
+
 
 logger = Logger.new
 logger.log_something 'wazzuuppp'
+
+
+# @variable - переменная объекта
+# def blabla -метод объекта (instance method)
+# end
+
+# @@variable  - переменная класса
+# def self.blabla -метод класса (static)
+# end
